6bcf657386b4cd5748bc7467f828b26c4a47bafa
LazyKitKat <lazykitkat@protonmail.com>
2022-03-11 10:31:46 +0100

    Added credits to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # binary-search-tree
+
+This project was made for [The Odin Project](www.theodinproject.org) Ruby course. 
+
+# Credits
+
+The ``pretty print`` method was written by the community at TOP's Discord channel.
--- lib/tree.rb
+++ lib/tree.rb
@@ -55,6 +55,10 @@ class Tree
         end
     end
 
+    def delete(val)
+        
+    end
+
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
